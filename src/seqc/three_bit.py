644cd58c52bb525e05626cfd814dc799736f9c80
Rami Eitan <re2337@columbia.edu>
2015-11-10 19:20:29 -0500

    updated error_correction to work woth ReadArray

--- src/seqc/three_bit.py
+++ src/seqc/three_bit.py
@@ -68,6 +68,21 @@ class ThreeBit:
                 tmp >>= 3
             res += num
         return res
+    
+    @staticmethod
+    def rmt_from_int(seq):
+        '''Extract the rmt (6 chrs) from an encoded sequecne'''
+        return seq&0o777777
+
+    @staticmethod
+    def c2_from_int(seq):
+        '''Extract barcode2 from a sequence'''
+        return (seq&0o77777777000000)>>(6*3)
+
+    @staticmethod
+    def c1_from_int(seq):
+        '''Extract barcode1 from a sequence'''
+        return seq>>((8+6)*3)
 
     @staticmethod
     def gc_content(s):
