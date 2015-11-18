00204e821cbfedac746eb4094f76f9388ada59dc
Kristy Choi <kristyc@dyn-160-39-142-230.dyn.columbia.edu>
2015-11-18 13:53:37 -0500

    weird paramiko exception handling

--- src/scripts/ssh_utils.py
+++ src/scripts/ssh_utils.py
@@ -1,45 +1,54 @@
 import paramiko
 import sys
 import time
-import boto3 #trying this here
-import seqc.io_lib as iolib
+import boto3
+import cluster_utils as clustutils
+# import seqc.io_lib as iolib #implement download files
 
 class SSHServer(object):
 
     def __init__(self, inst_id, keypath):
-        #TODO think about class args
-        #private_key = ~/.ssh/eyc2120.rsa, path to RSA key
-        #do you want to make this part of a separate function?
-        # self.ec2 = boto3.resource('ec2')
-        # self.instance = self.ec2.Instance(inst_id)
         ec2 = boto3.resource('ec2')
         self.instance = ec2.Instance(inst_id)
         self.key = keypath
         self.ssh = paramiko.SSHClient()
-        # if not private_key:
-        #     print('private rsa key required!')
-        #     sys.exit(2)
-        #if not private key, raise some kind of descriptive error
 
     def connect(self):
         max_attempts = 5
+        print('warming up the instance for connection, please wait...')
+        time.sleep(45) #give instance time to warm up
         self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         dns = self.instance.public_dns_name
         for attempt in range(max_attempts):
             try:
                 self.ssh.connect(dns, username='ubuntu', key_filename=self.key)
+            # except paramiko.AuthenticationException:
+            #     print('autherror')
+            #     print('instance not ready for connection, sleeping...')
+            #     self.instance.reload()
+            #     time.sleep(30)
+            # except paramiko.SSHException:
+            #     print('ssherror')
+            #     print('instance not ready for connection, sleeping...')
+            #     self.instance.reload()
+            #     time.sleep(30)
+            # except FileNotFoundError:
+            #     print('the key %s was not found!' %self.key)
+            #     sys.exit(2)
+            # except paramiko.BadHostKeyException:
+            #     print('the host key %s could not be verified!' %self.key)
+            #     sys.exit(2)
             except Exception as e:
                 print(e)
-                print('instance not ready for connection, sleeping...')
-                self.instance.reload()
+                print(sys.exc_info()[:2])
+                print('sleeping...')
                 time.sleep(30)
-            # except TimeoutError:
-            #     print('the connection timed out')
-            #     time.sleep(30)
-            # except TypeError:
-            #     print('weird type error?')
-            #     time.sleep(30)
-        # raise RuntimeError("maximum number of unsuccessful attempts reached")
+                # continue
+        # gname = self.instance.security_groups[0]['GroupName']
+        # gid = self.instance.security_groups[0]['GroupId']
+        # self.instance.terminate()
+        # boto3.client('ec2').delete_security_group(GroupName=gname,GroupId=gid)
+        # raise RuntimeError("connection failed: maximum number of unsuccessful attempts reached")
 
     def is_connected(self):
         if self.ssh.get_transport() is None:
@@ -55,21 +64,18 @@ class SSHServer(object):
     def download_files(self):
         raise NotImplementedError
 
-        # it says you need to create and connect to a transport:
-        # transport = paramiko.Transport((host, port))
-        # transport.connect(username = username, pkey = mykey)
-        # which is also what starcluster did, but what's the difference between that
-        # and an SFTPClient? (should look up)
-
     def get_file(self, localfile, remotefile):
+        if not self.is_connected():
+            print('you are not connected!')
+            sys.exit(2)
         ftp = self.ssh.open_sftp()
         ftp.get(remotefile, localfile)
         ftp.close()
-        # OSError if socket is closed, can get as many files as you want
-
-    # SFTP is more restrictive, expects full path of file location --> no wildcards
 
     def put_file(self, localfile, remotefile):
+        if not self.is_connected():
+            print('you are not connected!')
+            sys.exit(2)
         ftp = self.ssh.open_sftp()
         ftp.put(localfile, remotefile)
         ftp.close()
@@ -79,14 +85,7 @@ class SSHServer(object):
             print('you are not connected!')
             sys.exit(2)
         stdin, stdout, stderr = self.ssh.exec_command(args)
-        stdin.flush() #--> doesn't seem to be absolutely necessary
-        # if stderr.read():
-        #     print('ERROR: %s' % stderr.read().decode())
-        #     sys.exit(2)
+        stdin.flush()
         data = stdout.read().decode().splitlines()  # response in bytes
         errs = stderr.read().decode().splitlines()
-        # for line in data:
-        #     print(line)
-        #option to return or print (user-def)
         return data, errs
-
