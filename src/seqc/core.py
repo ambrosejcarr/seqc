1e632f8d45d0b95b0d45c35b11990ae04e499828
Kristy Choi <kristy.choi24@gmail.com>
2015-12-07 12:25:41 -0500

    edit

--- src/seqc/core.py
+++ src/seqc/core.py
@@ -238,10 +238,10 @@ def run_remote(kwargs: dict) -> None:
     print('cmd: %s' %cmd)
 
     # set up remote cluster here, finishes all the way through gitpull
-    # cluster = seqc.cluster_utils.ClusterServer()
-    # cluster.cluster_setup(clustname)
-    # cluster.serv.connect()
-    # seqc.log.info('Remote server set-up complete.')
+    cluster = seqc.cluster_utils.ClusterServer()
+    cluster.cluster_setup(clustname)
+    cluster.serv.connect()
+    seqc.log.info('Remote server set-up complete.')
 
     # todo | decide how we want to clean up
     temp_path = seqc.__path__[0]
