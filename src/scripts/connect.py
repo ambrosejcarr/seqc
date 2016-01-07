573eb0e27e039cf968b1f717f7de78dd996e533f
Kristy Choi <kristy.choi24@gmail.com>
2016-01-07 14:53:47 -0500

    simplified things

--- src/scripts/connect.py
+++ src/scripts/connect.py
@@ -1,35 +1,28 @@
 #!/usr/local/bin/python3
-__author__ = 'kristychoi'
 
 import boto3
-from subprocess import call, check_output
-import re
+from subprocess import call
 import seqc
 import configparser
 import os
+import sys
 
+inst_file = '/'.join(seqc.__file__.split('/')[:-3]) + '/src/scripts/instance.txt'
+try:
+    with open(inst_file, 'r') as f:
+        inst_id = f.readline().strip('\n')
+except FileNotFoundError:
+    print('Error: Please check that the cluster was set up appropriately!')
+    sys.exit(1)
+
+# extract hostname and key information
 ec2 = boto3.resource('ec2')
-with open('instance.txt') as f:
-    inst_id = f.readline().strip('\n')
 instance = ec2.Instance(inst_id)
 hostname = 'ubuntu@' + instance.public_dns_name
 config_file = '/'.join(seqc.__file__.split('/')[:-3]) + '/config'
 config = configparser.ConfigParser()
 config.read(config_file)
 keypath = os.path.expanduser(config['key']['rsa_key_location'])
-call(['ssh', '-i', keypath, hostname])
 
-# todo | see if you even need the last part
-# line = ' '.join(check_output(['grep', '-i', 'i-[a-z0-9]', 'seqc.log']).decode().split())
-# result = re.search('i-[a-z0-9]+', line)
-# if not result:
-#     print('')
-# else:
-#     inst_id = result.group(0)
-#     instance = ec2.Instance(inst_id)
-#     hostname = 'ubuntu@' + instance.public_dns_name
-#     config_file = '/'.join(seqc.__file__.split('/')[:-3]) + '/config'
-#     config = configparser.ConfigParser()
-#     config.read(config_file)
-#     keypath = os.path.expanduser(config['key']['rsa_key_location'])
-#     call(['ssh', '-i', keypath, hostname])
+# connect to instance
+call(['ssh', '-i', keypath, hostname])
