9ac3e8e2c4ca9f6d76f867b08753d1e17a8345e7
Kristy Choi <kristy.choi24@gmail.com>
2016-01-07 14:32:46 -0500

    reading instance.txt file instead of seqc.log

--- src/scripts/connect.py
+++ src/scripts/connect.py
@@ -9,16 +9,27 @@ import configparser
 import os
 
 ec2 = boto3.resource('ec2')
-line = ' '.join(check_output(['grep', '-i', 'i-[a-z0-9]', 'seqc.log']).decode().split())
-result = re.search('i-[a-z0-9]+', line)
-if not result:
-    print('')
-else:
-    inst_id = result.group(0)
-    instance = ec2.Instance(inst_id)
-    hostname = 'ubuntu@' + instance.public_dns_name
-    config_file = '/'.join(seqc.__file__.split('/')[:-3]) + '/config'
-    config = configparser.ConfigParser()
-    config.read(config_file)
-    keypath = os.path.expanduser(config['key']['rsa_key_location'])
-    call(['ssh', '-i', keypath, hostname])
+with open('instance.txt') as f:
+    inst_id = f.readline().strip('\n')
+instance = ec2.Instance(inst_id)
+hostname = 'ubuntu@' + instance.public_dns_name
+config_file = '/'.join(seqc.__file__.split('/')[:-3]) + '/config'
+config = configparser.ConfigParser()
+config.read(config_file)
+keypath = os.path.expanduser(config['key']['rsa_key_location'])
+call(['ssh', '-i', keypath, hostname])
+
+# todo | see if you even need the last part
+# line = ' '.join(check_output(['grep', '-i', 'i-[a-z0-9]', 'seqc.log']).decode().split())
+# result = re.search('i-[a-z0-9]+', line)
+# if not result:
+#     print('')
+# else:
+#     inst_id = result.group(0)
+#     instance = ec2.Instance(inst_id)
+#     hostname = 'ubuntu@' + instance.public_dns_name
+#     config_file = '/'.join(seqc.__file__.split('/')[:-3]) + '/config'
+#     config = configparser.ConfigParser()
+#     config.read(config_file)
+#     keypath = os.path.expanduser(config['key']['rsa_key_location'])
+#     call(['ssh', '-i', keypath, hostname])
