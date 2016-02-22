5f233479d9b7a41719f026b36278e6f6adab623b
ajc <ambrosejcarr@gmail.com>
2016-02-22 12:38:00 -0500

    tested test_result_equal.py

--- src/scripts/test_result_equal.py
+++ src/scripts/test_result_equal.py
@@ -1,23 +1,40 @@
-#!/usr/local/bin/python
+#!/usr/local/bin/python3
 
 import pickle
-import hashlib
 import sys
 import numpy as np
 
-# import numpy object
 
-def main(_, old, new):
+def main(old, new):
     with open(old, 'rb') as pickle_object:
         old_counts = pickle.load(pickle_object)
     with open(new, 'rb') as pickle_object:
         new_counts = pickle.load(pickle_object)
 
-    # expect a coo matrix
+    assert np.array_equal(old_counts['molecules']['matrix'].data,
+                          new_counts['molecules']['matrix'].data)
+    assert np.array_equal(old_counts['molecules']['matrix'].row,
+                          new_counts['molecules']['matrix'].row)
+    assert np.array_equal(old_counts['molecules']['matrix'].col,
+                          new_counts['molecules']['matrix'].col)
+    assert np.array_equal(old_counts['molecules']['col_ids'],
+                          new_counts['molecules']['col_ids'])
+    assert np.array_equal(old_counts['molecules']['row_ids'],
+                          new_counts['molecules']['row_ids'])
 
-    old_digest = hashlib.sha256(old_counts).hexdigest()
-    new_digest = hashlib.sha256(new_counts).hexdigest()
-
+    assert np.array_equal(old_counts['reads']['matrix'].data,
+                          new_counts['reads']['matrix'].data)
+    assert np.array_equal(old_counts['reads']['matrix'].row,
+                          new_counts['reads']['matrix'].row)
+    assert np.array_equal(old_counts['reads']['matrix'].col,
+                          new_counts['reads']['matrix'].col)
+    assert np.array_equal(old_counts['reads']['col_ids'],
+                          new_counts['reads']['col_ids'])
+    assert np.array_equal(old_counts['reads']['row_ids'],
+                          new_counts['reads']['row_ids'])
 
 if __name__ == "__main__":
-    main(*sys.argv)
+    if not len(sys.argv) == 3:
+        print('usage: test_result_equal old_counts, new_counts')
+    else:
+        main(*sys.argv[1:])
