2871223ba3ec154da066198f747f0de5a1a258d1
ajc <ambrosejcarr@gmail.com>
2016-02-22 11:22:09 -0500

    added test results skeleton

--- src/scripts/test_result_equal.py
+++ src/scripts/test_result_equal.py
@@ -1,6 +1,15 @@
 #!/usr/local/bin/python
 
-import numpy as np
 import pickle
 import hashlib
+import sys
+import numpy as np
 
+# import numpy object
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main(*sys.argv)
