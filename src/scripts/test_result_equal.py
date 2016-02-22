0119cf20bcfd4dda5a0025c7cd380ad784b09971
ajc <ambrosejcarr@gmail.com>
2016-02-22 12:14:01 -0500

    removed old functions from seqc; added code to test_result_equal.py

--- src/scripts/test_result_equal.py
+++ src/scripts/test_result_equal.py
@@ -7,8 +7,16 @@ import numpy as np
 
 # import numpy object
 
-def main():
-    pass
+def main(_, old, new):
+    with open(old, 'rb') as pickle_object:
+        old_counts = pickle.load(pickle_object)
+    with open(new, 'rb') as pickle_object:
+        new_counts = pickle.load(pickle_object)
+
+    # expect a coo matrix
+
+    old_digest = hashlib.sha256(old_counts).hexdigest()
+    new_digest = hashlib.sha256(new_counts).hexdigest()
 
 
 if __name__ == "__main__":
