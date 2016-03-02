0aafd0f233cb754e9ee6999f1a0f7ed3992c1f67
AJC <mail@ambrosejcarr.com>
2016-03-02 11:53:25 -0500

    added ability to enter from merged fastq, samfile

--- src/scripts/SEQC_NEW.py
+++ src/scripts/SEQC_NEW.py
@@ -14,10 +14,15 @@ def parse_args():
                    choices=['in_drop', 'drop_seq', 'mars1_seq',
                             'mars2_seq', 'in_drop_v2'],
                    help='which platform are you merging annotations from?')
-    p.add_argument('-g', '--genomic-fastq', nargs='+', metavar='G',
+    p.add_argument('-g', '--genomic-fastq', nargs='*', metavar='G', default=[],
                    help='fastq file(s) containing genomic information')
-    p.add_argument('-b', '--barcode-fastq', nargs='?', metavar='B',
+    p.add_argument('-b', '--barcode-fastq', nargs='*', metavar='B', default=[],
                    help='fastq file(s) containing barcode information')
+    p.add_argument('-m', '--merged-fastq', nargs='?', metavar='M', default='',
+                   help='fastq file containing genomic information annotated with '
+                        'barcode data')
+    p.add_argument('-s', '--samfile', nargs='?', metavar='S', default='',
+                   help='sam file containing aligned, merged fastq records.')
     p.add_argument('-o', '--output-stem', metavar='O', help='file stem for output files '
                    'e.g. ./seqc_output/tumor_run5')
     p.add_argument('-i', '--index', metavar='I', help='Folder containing index files for '
@@ -47,25 +52,37 @@ def main():
     seqc.log.args(args)
     n_threads = multiprocessing.cpu_count() - 1
 
-    seqc.log.info('Merging genomic reads and barcode annotations.')
-    merge_function = getattr(seqc.sequence.merge_functions, args.platform)
-    merged = seqc.sequence.fastq.merge_paired(
-            merge_function=merge_function,
-            fout=args.output_stem + '_merged.fastq',
-            genomic=args.genomic_fastq,
-            barcode=args.barcode_fastq)
+    # determine where the script should start:
+    merge = True
+    align = True
+    if args.samfile:
+        merge = False
+        align = False
+    if args.merged_fastq:
+        merge = False
 
-    seqc.log.info('Aligning merged fastq records.')
-    *base_directory, stem = args.output_stem.split('/')
-    alignment_directory = '/'.join(base_directory) + '/alignments/'
-    os.makedirs(alignment_directory, exist_ok=True)
-    samfile = seqc.align.STAR.align(merged, args.index, n_threads, alignment_directory)
+    if merge:
+        seqc.log.info('Merging genomic reads and barcode annotations.')
+        merge_function = getattr(seqc.sequence.merge_functions, args.platform)
+        args.merged_fastq = seqc.sequence.fastq.merge_paired(
+                merge_function=merge_function,
+                fout=args.output_stem + '_merged.fastq',
+                genomic=args.genomic_fastq,
+                barcode=args.barcode_fastq)
+
+    if align:
+        seqc.log.info('Aligning merged fastq records.')
+        *base_directory, stem = args.output_stem.split('/')
+        alignment_directory = '/'.join(base_directory) + '/alignments/'
+        os.makedirs(alignment_directory, exist_ok=True)
+        args.samfile = seqc.align.STAR.align(
+                args.merged_fastq, args.index, n_threads, alignment_directory)
 
     seqc.log.info('Filtering alignments and constructing record database.')
     fc = seqc.convert_features.ConvertFeatureCoordinates.from_gtf(
             args.index + 'annotations.gtf',
             args.max_insert_size)
-    h5db = seqc.arrays.ReadArray.from_samfile(samfile, fc)
+    h5db = seqc.arrays.ReadArray.from_samfile(args.samfile, fc)
 
     seqc.log.info('Resolving ambiguous alignments.')
     # h5db.resolve_alignments_old(expectations=args.index + 'p_coalignment_array.p',
@@ -77,17 +94,17 @@ def main():
 
     seqc.log.info('Generating molecule x cell and read x cell matrices.')
     # mols, mrow, mcol = h5db.unique_features_to_sparse_counts(
-    mols, mrow, mcol = h5db.unique_features_to_sparse_counts_cmp(
-        collapse_molecules=True,
-        n_poly_t_required=args.min_poly_t)
-    # reads, rrow, rcol = h5db.unique_features_to_sparse_counts(
-    reads, rrow, rcol = h5db.unique_features_to_sparse_counts_cmp(
-        collapse_molecules=False,
-        n_poly_t_required=args.min_poly_t)
-    matrices = {'molecules': {'matrix': mols, 'row_ids': mrow, 'col_ids': mcol},
-                'reads': {'matrix': reads, 'row_ids': rrow, 'col_ids': rcol}}
-    with open(args.output_stem + '_read_and_mol_matrices.p', 'wb') as f:
-        pickle.dump(matrices, f)
+    # mols, mrow, mcol = h5db.unique_features_to_sparse_counts_cmp(
+    #     collapse_molecules=True,
+    #     n_poly_t_required=args.min_poly_t)
+    # # reads, rrow, rcol = h5db.unique_features_to_sparse_counts(
+    # reads, rrow, rcol = h5db.unique_features_to_sparse_counts_cmp(
+    #     collapse_molecules=False,
+    #     n_poly_t_required=args.min_poly_t)
+    # matrices = {'molecules': {'matrix': mols, 'row_ids': mrow, 'col_ids': mcol},
+    #             'reads': {'matrix': reads, 'row_ids': rrow, 'col_ids': rcol}}
+    # with open(args.output_stem + '_read_and_mol_matrices.p', 'wb') as f:
+    #     pickle.dump(matrices, f)
     matrices = h5db.to_sparse_counts_diff(required_support=2, min_poly_t=args.min_poly_t)
     with open(args.output_stem + '_read_and_mol_matrices_diff.p', 'wb') as f:
         pickle.dump(matrices, f)
