61667c3248975085cfb0824564292de99bae6637
kristychoi <kristy.choi24@gmail.com>
2016-03-18 01:56:18 -0400

    writing in subnet

--- src/seqc/remote.py
+++ src/seqc/remote.py
@@ -71,8 +71,7 @@ class ClusterServer(object):
         self.keypath = os.path.expanduser(config['key']['rsa_key_location'])
         self.image_id = config[template]['node_image_id']
         self.inst_type = config[template]['node_instance_type']
-        if template == 'c4':
-            self.subnet = config[template]['subnet_id']
+        self.subnet = config[template]['subnet_id']
         self.zone = config[template]['availability_zone']
         self.n_tb = config['raid']['n_tb']
         self.dir_name = config['gitpull']['dir_name']
@@ -81,6 +80,7 @@ class ClusterServer(object):
 
         # check that config file is complete
         attrs = vars(self)
+        print(attrs)
         if None in attrs.values():
             raise ValueError('Config file is incomplete! Exiting.')
 
