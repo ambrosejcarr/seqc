e2913a527e541134a7f11729d6ae2f8b02a9420f
kristychoi <kristy.choi24@gmail.com>
2016-03-24 14:56:08 -0400

    testing

--- src/seqc/remote.py
+++ src/seqc/remote.py
@@ -140,7 +140,7 @@ class ClusterServer(object):
 
     def create_volume(self):
         """creates a volume of size vol_size and returns the volume's id"""
-        vol_size = 1024
+        vol_size = 50 #1024
         vol = self.ec2.create_volume(Size=vol_size, AvailabilityZone=self.zone,
                                      VolumeType='standard')
         vol_id = vol.id
