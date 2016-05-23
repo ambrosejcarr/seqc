fc25f58d4973609b58e8107801b79247dcdaea77
kristychoi <kristy.choi24@gmail.com>
2016-05-23 17:13:18 -0400

    fixed if statement for aws instances

--- src/seqc/remote.py
+++ src/seqc/remote.py
@@ -82,7 +82,7 @@ class ClusterServer(object):
 
     def create_cluster(self):
         """creates a new AWS cluster with specifications from config"""
-        if 'c4' or 'r3' in self.inst_type:
+        if 'c4' in self.inst_type or 'r3' in self.inst_type:
             if not self.subnet:
                 raise ValueError('A subnet-id must be specified for C4 instances!')
             else:
@@ -94,16 +94,13 @@ class ClusterServer(object):
                                                       'AvailabilityZone': self.zone},
                                                   SecurityGroupIds=[self.sg],
                                                   SubnetId=self.subnet)
-        elif 'c3' in self.inst_type:
+        else:  # c3 instance
             clust = self.ec2.create_instances(ImageId=self.image_id, MinCount=1,
                                               MaxCount=1,
                                               KeyName=self.keyname,
                                               InstanceType=self.inst_type,
                                               Placement={'AvailabilityZone': self.zone},
                                               SecurityGroupIds=[self.sg])
-
-        else:
-            raise ValueError('self.inst_type must be a c3, c4, or r3 instance')
         instance = clust[0]
         seqc.log.notify('Created new instance %s. Waiting until instance is running' %
                         instance)
