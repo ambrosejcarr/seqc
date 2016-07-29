b3ce0e0e27ad6bc51b409e67f08e14160c44d8f4
Cyril Cros <cyril.cros@polytechnique.org>
2016-07-28 20:35:46 -0400

    Cyril remote integration to develop (#186)
    * changed master to pull from master instead of develop
    
    * Raising cleaner exceptions, minor refactoring
    
    * Minor reformatting, altering wrapper for reuse
    
    * Better error handling, using Boto3 multiple attempt wrapper
    
    * Corrected wrapper to use single layer
    
    * Debugging various issues- remote seems stable
    
    * Adding stuff from remote
    
    * Further changes from develop - log.py
    
    * Better logging, correcting names of remote function
    
    * More testing and error handling, everything passed
    
    * More testing and error handling, need testing
    
    * Removing user specific stuff; all relevant tests passed
    
    * Fixing instance state detection
    
    * Correcting retry_boto_call verbosity

--- src/seqc/exceptions.py
+++ src/seqc/exceptions.py
@@ -35,38 +35,38 @@ class ArgumentParserError(Exception):
 class SparseMatrixError(Exception):
     pass
 
+
 class EmptyMatrixError(Exception):
     pass
 
 
-@contextmanager
-def boto_errors(ident=None):
-    """context manager that traps and retries boto functions
-    to prevent random failures -- usually during batch runs
-    :param ident: name of boto call"""
-
-    try:
-        yield
-    except Exception:
-        if ident:
-            seqc.log.notify('Error in ' + ident + ', retrying in 5s...')
-        else:
-            seqc.log.notify('Error during boto call, retrying in 5s...')
-        time.sleep(5)
-
-
-def retry_boto_call(func, retries=3):
-    """handles unexpected boto3 behavior, retries (default 3x)
+def retry_boto_call(func, retries=3, exceptions_to_catch=Exception, exception_to_raise: Exception = None,
+                    delay_retry=5, verbose=False):
+    """improved wrapper to avoid issues with Boto3 by retrying an operation until success
     :param func: boto call to be wrapped
-    :param retries: total # tries to re-call boto function"""
+    :param retries: total # tries to re-call boto function
+    :param exceptions_to_catch: an Exception or tuple of Exception that will trigger a retry
+    :param exception_to_raise: raise this after repeated failures
+    :param delay_retry: waiting time before new attempt
+    """
 
     @wraps(func)
     def wrapper(*args, **kwargs):
-        numtries = retries
-        while numtries > 0:
-            with boto_errors(func.__name__):
+        num_tries = retries
+        while num_tries > 0:
+            try:
                 return func(*args, **kwargs)
-            numtries -= 1
-            if numtries == 0:
-                raise BotoCallError('Unresolvable error in boto call, exiting.')
+            except exceptions_to_catch as e:
+                if verbose:
+                    seqc.log.notify("Non fatal error in function "+func.__qualname__+" --> "+str(e) +
+                                    " | retrying in {nbr}s".format(nbr=delay_retry))
+                num_tries -= 1
+                if num_tries == 0:
+                    real_exception = BotoCallError('Boto3 failed repeatedly.') \
+                        if not exception_to_raise else exception_to_raise
+                    seqc.log.notify("SeqC encountered repeated failures while trying to execute"
+                                    "{method}. Giving up after {nbr_tries} with {sec}s spacing."
+                                    .format(method=func.__qualname__, nbr_tries=retries, sec=delay_retry))
+                    raise real_exception
+                time.sleep(delay_retry)
     return wrapper
