1fdd4e4d2043f6299b68d9dfedd4f483d804e7e1
Ambrose J Carr <mail@ambrosejcarr.com>
2016-11-27 12:53:11 -0500

    removed superfluous basespace function from download, now using io.basespace directly. Passing tests, ready for merge.

--- src/seqc/core/download.py
+++ src/seqc/core/download.py
@@ -1,27 +1,5 @@
 import os
-from subprocess import Popen
 from seqc import io, log
-
-
-# todo fold this module into io.S3 and io.Basespace
-def basespace(args, output_dir: str, basespace_token: str) -> (str, str):
-    """
-    If --basespace argument is passed, download BaseSpace data
-
-    :param args: Namespace object, output of ArgumentParser.parse_args()
-    :param output_dir: str, output directory
-    :param basespace_token: str, OAuth token for BaseSpace authentication
-    :return barcode_fastq, genomic_fastq:
-    """
-    log.info('BaseSpace link provided for fastq argument. Downloading input data.')
-
-    # making extra directories for BaseSpace download
-    bspace_dir = output_dir + '/Data/Intensities/BaseCalls/'
-    bf = Popen(['mkdir', '-p', bspace_dir])
-    bf.communicate()
-    barcode_fastq, genomic_fastq = io.BaseSpace.download(
-        args.platform, args.basespace, output_dir, basespace_token)
-    return barcode_fastq, genomic_fastq
 
 
 def s3_data(files_or_links, output_prefix):
